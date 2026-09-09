ca17b5022462701e3605547c9b2addaebbe819e8
ponkavyaponboopathiraj <ponkavya2002@gmail.com>
2026-09-09 19:18:33 +0530

    Adding permission repoo file

---
+++ iam_backend/src/main/java/IAM/IAM/repository/PermissionRepository.java
@@ -0,0 +1,13 @@
+package IAM.IAM.repository;
+
+import IAM.IAM.entity.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+
+    Optional<Permission> findByName(String name);
+
+    boolean existsByName(String name);
+}
