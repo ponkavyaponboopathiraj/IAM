faab891d947ff2b8f70d65d9a899b6e707c13db1
ponkavyaponboopathiraj <ponkavya2002@gmail.com>
2026-09-10 14:33:58 +0530

    Controller file added

---
+++ iam_backend/src/main/java/IAM/IAM/controller/AuthController.java
@@ -0,0 +1,29 @@
+package IAM.IAM.controller;
+
+import IAM.IAM.entity.User;
+import IAM.IAM.service.UserService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+
+        User registeredUser = userService.registerUser(user);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(registeredUser);
+    }
+}
