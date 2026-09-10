50585759891514a6312ae4fff18922d0033c9b84
ponkavyaponboopathiraj <ponkavya2002@gmail.com>
2026-09-10 15:45:28 +0530

    Adding config file

---
+++ iam_backend/src/main/java/IAM/IAM/config/AuthorizationServerConfig.java
@@ -0,0 +1,28 @@
+package IAM.IAM.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AuthorizationServerConfig {
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(
+            HttpSecurity http) throws Exception {
+
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
+        http
+            .oauth2ResourceServer(resourceServer ->
+                resourceServer.jwt(Customizer.withDefaults())
+            );
+
+        return http.build();
+    }
+}
